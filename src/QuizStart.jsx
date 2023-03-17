2b0d6a79c6771b6a9eafbf4add2dbba12bef804f
Lieven <lieven.vaneeckhaute@gmail.com>
2023-03-17 21:40:32 +0100

    format

--- src/QuizStart.jsx
+++ src/QuizStart.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-const Quiz = ({data}) => {
+const Quiz = ({ data }) => {
 
     const calculate = () => {
         let score = 0;
         for (let dataElement of data) {
-            const val = document.getElementById("quiz-"+dataElement.id).value;
+            const val = document.getElementById("quiz-" + dataElement.id).value;
             if (val === dataElement.answer) {
                 score++;
             }
@@ -17,12 +17,12 @@ const Quiz = ({data}) => {
     }
     const [correctAnswers, setCorrectAnswers] = useState(0);
 
-    return <> 
-    {data.map(e => <>
-        <p> {e.question}</p> 
-        <input type="text" key={e.id} id={"quiz-"+e.id} question={e.question} answer={e.answer} />
-    </>)}
-    <p>Answers correct {correctAnswers} / {data.length}</p>
+    return <>
+        {data.map(e => <>
+            <p> {e.question}</p>
+            <input type="text" key={e.id} id={"quiz-" + e.id} question={e.question} answer={e.answer} />
+        </>)}
+        <p>Answers correct {correctAnswers} / {data.length}</p>
         <button onClick={calc}>Submit answers</button>
     </>;
 }
@@ -42,14 +42,14 @@ const convertData = (data, sep) => {
     return list;
 }
 
-const QuizStart = ()  => {
-    const [quizing, setQuizing]=useState(false);
-    const [isCaseSensitive, setCaseSensitive]=useState();
-    const [randomizeOrder, setRandomizeOrder]=useState(true);
-    const [questionOnLeft, setQuestionOnLeft]=useState(true);
-    const [uriQuestion, setUriQuestion]=useState(true);
-    const [separator, setSeparator]=useState(",");
-    const [data, setData]=useState("");
+const QuizStart = () => {
+    const [quizing, setQuizing] = useState(false);
+    const [isCaseSensitive, setCaseSensitive] = useState();
+    const [randomizeOrder, setRandomizeOrder] = useState(true);
+    const [questionOnLeft, setQuestionOnLeft] = useState(true);
+    const [uriQuestion, setUriQuestion] = useState(true);
+    const [separator, setSeparator] = useState(",");
+    const [data, setData] = useState("");
 
     const questionsAndStuff = convertData(data, separator);
 
@@ -62,14 +62,14 @@ const QuizStart = ()  => {
     };
     return <>
         {!quizing && (<><textarea rows={10} cols={100} onChange={handleChangeData}>{data}</textarea>
-        <label><input type="checkbox" checked={isCaseSensitive} onChange={() => setCaseSensitive(!isCaseSensitive)}/>Case sensitive</label>
-        <label><input type="checkbox" checked={randomizeOrder} onChange={() => setRandomizeOrder(!randomizeOrder)}/>Randomize order</label>
-        <label><input type="checkbox" checked={questionOnLeft}  onChange={() => setQuestionOnLeft(!questionOnLeft)}/>Question on left</label>
-        <label><input type="checkbox" checked={uriQuestion} onChange={() => setUriQuestion(!uriQuestion)}/>Load Question as image if URI</label>
-        <label>Separated by <input type="text" value={separator} onChange={handleChangeSeparator}/></label>
-        <button onClick={() => setQuizing(true)}>Start Quiz</button></>)}
-        {quizing && <Quiz data={questionsAndStuff}>/</Quiz> }
+            <label><input type="checkbox" checked={isCaseSensitive} onChange={() => setCaseSensitive(!isCaseSensitive)} />Case sensitive</label>
+            <label><input type="checkbox" checked={randomizeOrder} onChange={() => setRandomizeOrder(!randomizeOrder)} />Randomize order</label>
+            <label><input type="checkbox" checked={questionOnLeft} onChange={() => setQuestionOnLeft(!questionOnLeft)} />Question on left</label>
+            <label><input type="checkbox" checked={uriQuestion} onChange={() => setUriQuestion(!uriQuestion)} />Load Question as image if URI</label>
+            <label>Separated by <input type="text" value={separator} onChange={handleChangeSeparator} /></label>
+            <button onClick={() => setQuizing(true)}>Start Quiz</button></>)}
+        {quizing && <Quiz data={questionsAndStuff}>/</Quiz>}
     </>;
-    
+
 }
 export default QuizStart;
