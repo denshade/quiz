ccb81e7195c4a7ed5cb5e89c79be643125b67c3f
Lieven <lieven.vaneeckhaute@gmail.com>
2023-03-24 21:51:31 +0100

    rows and cols

--- src/QuizStart.jsx
+++ src/QuizStart.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import FormCheckInput from 'react-bootstrap/FormCheckInput';
 import styled from 'styled-components';
 
@@ -32,6 +34,7 @@ margin: 5px;
 `;
 
 const Quiz = ({ data, setQuizing, isCaseSensitive }) => {
+    const [correctAnswers, setCorrectAnswers] = useState(0);
 
     const calculate = () => {
         let score = 0;
@@ -53,8 +56,6 @@ const Quiz = ({ data, setQuizing, isCaseSensitive }) => {
     const calc = () => {
         setCorrectAnswers(calculate());
     }
-    const [correctAnswers, setCorrectAnswers] = useState(0);
-
     return <>
         {data.map(e => <Question>
             {e.question.startsWith("http") && <img src={e.question} key={"p"+e.id}/>}
@@ -91,7 +92,7 @@ const convertData = (data, sep, randomizeOrder, swapQuestion, nrQuestions) => {
     for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
         var line = lines[lineIndex];
         var lineArr = line.split(sep);
-        if (lineArr.length != 2) continue;
+        if (lineArr.length !== 2) continue;
         var obj = {};
         obj.id = lineIndex;
         if (swapQuestion) {
@@ -115,7 +116,7 @@ const QuizStart = () => {
     const [matchAnyOrder, setMatchAnyOrder] = useState(true);
     const [separator, setSeparator] = useState(",");
     const [nrQuestions, setNrQuestions] = useState("");
-    const [data, setData] = useState("");
+    const [data, setData] = useState("1+1=?,2\n2+2=?,4");
 
     const questionsAndStuff = convertData(data, separator, randomizeOrder, swapQuestion, nrQuestions);
 
@@ -145,9 +146,23 @@ const QuizStart = () => {
                 <label><FormCheckInput checked={swapQuestion} onChange={() => setQuestionOnLeft(!swapQuestion)} /> Swap question and answer</label>
                 <label><FormCheckInput checked={matchAnyOrder} onChange={() => setMatchAnyOrder(!matchAnyOrder)} /> Match words in any order</label>
                 <label><FormCheckInput checked={uriQuestion} onChange={() => setUriQuestion(!uriQuestion)} /> Load Question as image if URI</label>
-                <label>Separated by <input type="text" value={separator} onChange={handleChangeSeparator} /></label>
-                <label>#Questions to ask (leave empty for all questions) <input type="text" value={nrQuestions} onChange={handleChangeQuestions} /></label>
-            </Options>
+                <Row>
+                    <Col>
+                    Separated by 
+                    </Col>
+                    <Col>
+                        <input type="text" value={separator} onChange={handleChangeSeparator} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    #Questions to ask (leave empty for all questions) 
+                    </Col>
+                    <Col>
+                        <input type="number" value={nrQuestions} onChange={handleChangeQuestions} />
+                    </Col>
+                </Row>
+        </Options>
             <MyButton onClick={() => setQuizing(true)}>Start Quiz</MyButton></>)}
         {quizing && <Quiz setQuizing={setQuizing} isCaseSensitive={isCaseSensitive} separator={separator} data={questionsAndStuff}>/</Quiz>}
     </>;
